420ab30c2d6c7a2d980359f07b527d132029805e
Dorst <carlos.dorst@azion.com>
2021-11-30 13:34:50 -0300

    Add teleport example

---
+++ examples/teleport.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "XXXX"
+  secret_key = "XXX"
+}
+
+module "teleport_server" {
+  source = "git::https://github.com/cemdorst/terraform-modules.git//terraform-aws-teleport"
+  cluster_name = "teleport"
+  number_of_instances = 1
+}
