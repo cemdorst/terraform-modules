546d92de6a509a3f30445b55bcdfff32fae42c65
Dorst <carlos.dorst@azion.com>
2021-12-29 23:24:25 -0300

    [fix] set empty default value for a_records

--- terraform-aws-route53/main.tf
+++ terraform-aws-route53/main.tf
@@ -6,7 +6,7 @@ resource "aws_route53_record" "this" {
   for_each = var.a_records
   zone_id  = aws_route53_zone.this.id
   name     = each.value
-  records  = ["$each.key}"]
+  records  = ["${each.key}"]
   type     = "A"
   ttl      = "300"
 }
