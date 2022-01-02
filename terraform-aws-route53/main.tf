1250c3053d1115d5a087cd1d287a78f529d10349
Dorst <carlos.dorst@azion.com>
2022-01-02 15:40:56 -0300

    [feat] add support for type cname records

--- terraform-aws-route53/main.tf
+++ terraform-aws-route53/main.tf
@@ -2,11 +2,20 @@ resource "aws_route53_zone" "this" {
   name = var.zone_name
 }
 
-resource "aws_route53_record" "this" {
+resource "aws_route53_record" "a" {
   for_each = var.a_records
   zone_id  = aws_route53_zone.this.id
   name     = each.key
   records  = ["${each.value}"]
   type     = "A"
+  ttl      = "300"
+}
+
+resource "aws_route53_record" "cname" {
+  for_each = var.cname_records
+  zone_id  = aws_route53_zone.this.id
+  name     = each.key
+  records  = ["${each.value}"]
+  type     = "CNAME"
   ttl      = "300"
 }
