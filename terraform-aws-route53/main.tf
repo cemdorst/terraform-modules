ece81bb32e8f19a7509a43ff303ee018cbe1222b
Dorst <carlos.dorst@azion.com>
2022-01-09 23:32:04 -0300

    [feat] add CAA and AAAA type records

--- terraform-aws-route53/main.tf
+++ terraform-aws-route53/main.tf
@@ -11,6 +11,15 @@ resource "aws_route53_record" "a" {
   ttl      = "300"
 }
 
+resource "aws_route53_record" "aaaa" {
+  for_each = var.aaaa_records
+  zone_id  = aws_route53_zone.this.id
+  name     = each.key
+  records  = ["${each.value}"]
+  type     = "AAAA"
+  ttl      = "300"
+}
+
 resource "aws_route53_record" "cname" {
   for_each = var.cname_records
   zone_id  = aws_route53_zone.this.id
@@ -26,5 +35,14 @@ resource "aws_route53_record" "txt" {
   name     = each.key
   records  = each.value
   type     = "TXT"
+  ttl      = "300"
+}
+
+resource "aws_route53_record" "caa" {
+  for_each = var.caa_records
+  zone_id  = aws_route53_zone.this.id
+  name     = each.key
+  records  = each.value
+  type     = "CAA"
   ttl      = "300"
 }
